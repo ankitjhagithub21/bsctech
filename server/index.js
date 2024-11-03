8c9e5c4118ce035f86fa5fe8fe3cbea48261000d
ankitjhagithub21 <ajha45075@gmail.com>
2024-11-04 01:12:57 +0530

    fixes cors issue

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ const port = process.env.PORT || 5000
 connectDB()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin:process.env.ORIGIN,
+  credentials:true
+}))
 
 
 app.get('/', (req, res) => {
